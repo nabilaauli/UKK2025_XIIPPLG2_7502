74cef188bab628fcfef3fc4a8cb624e267effcfb
nabilaauli <dangelolochlanhobi@gmail.com>
2025-02-13 10:52:06 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+export default function App() {
+
+  return(
+    <>
+    <Typography variant="h1">pooooo</Typography>
+    </>
+  )
+}
+
---
+++ src/theme/color.jsx
@@ -0,0 +1,29 @@
+export const deepPurple = {
+    50: "#e0f2f1",
+    200: "#b39ddb",
+    300: "#9575cd",
+    400: "#7e57c2",
+    500: "#673ab7",
+    600: "#5e35b1",
+    700: "#512da8",
+    800: "#4527a0",
+    900: "#311b92",
+  };
+  export const dark = {
+    100: "#E9E9E9",
+    200: "#D3D3D3",
+    300: "#1E1E1E",
+    400: "#696262",
+    500: "#1E2126",
+    600: "#D9D9D9",
+  };
+  export const light = {
+    100: "#FFFFFF",
+    200: "#F4F4F4",
+  };
+  export const succes = {
+    100: "#6FE29D",
+  };
+  export const danger = {
+    100: "#F56767",
+  };
